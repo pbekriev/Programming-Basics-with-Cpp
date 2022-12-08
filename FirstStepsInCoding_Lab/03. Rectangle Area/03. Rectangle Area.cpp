4d198e6b98728d44e014a59efbd0974874de3003
pbekriev <petar.bekriev@gmail.com>
2022-12-08 16:35:28 +0200

    03. Rectangle Area

---
+++ FirstStepsInCoding_Lab/03. Rectangle Area/03. Rectangle Area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number_a;
+	int number_b;
+	cin >> number_b >> number_a;
+	int area;
+	area = number_a * number_b;
+	cout << area;
+
+	return 0;
+}
